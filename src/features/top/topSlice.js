79216b423aec9cb1b11d74577d5bb76ab09885e1
drdevlin <drdevlin@fastmail.com>
2020-12-16 15:43:01 -0500

    Add fetch thunk to Top slice

--- src/features/top/topSlice.js
+++ src/features/top/topSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { redditGet } from '../../api/reddit';
 
 const initialState = {
   posts: [
@@ -6,12 +7,32 @@ const initialState = {
     {title: 'bar'},
     {title: 'baz'},
   ],
+  status: 'idle',
+  error: null
 };
+
+export const fetchPosts = createAsyncThunk('top/fetchPosts', async () => {
+  const response = await redditGet('/r/toronto/top?t=day');
+  return response.data.children; // Returns just the posts array
+})
 
 export const topSlice = createSlice({
   name: 'top',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchPosts.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [fetchPosts.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+      state.posts = action.payload;
+    },
+    [fetchPosts.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    }
+  },
 });
 
 export default topSlice.reducer;
