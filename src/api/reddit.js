63678c5d00b4344e5c1bc40bcb02e22f15f13d50
drdevlin <drdevlin@fastmail.com>
2020-12-16 15:41:13 -0500

    Create fetch function for getting posts

--- src/api/reddit.js
+++ src/api/reddit.js
@@ -1,4 +1,4 @@
-import { basicAuth, appSignature } from './secrets';
+import { basicAuth } from './secrets';
 
 const tokenURL = 'https://www.reddit.com/api/v1/access_token';
 const baseURL = 'https://oauth.reddit.com';
@@ -8,29 +8,19 @@ const tokenConfig = {
   headers: {
     'Authorization': `Basic ${basicAuth}`,
     'Content-Type': 'application/x-www-form-urlencoded',
-    'User-Agent': appSignature
   }
 };
 
-async function request(endpoint, { body, ...customConfig } = {}) {
-  const config = {
-    method: body ? 'POST' : 'GET',
-    ...customConfig,
-    headers: {
-      ...customConfig.headers,
-    },
-  }
-
-  let data
+async function request(endpoint, config) {
   try {
-    const response = await window.fetch(endpoint, config)
-    data = await response.json()
+    const response = await fetch(endpoint, config)
     if (response.ok) {
-      return data
+      const jsonResponse = await response.json();
+      return jsonResponse;
     }
     throw new Error(response.statusText)
-  } catch (err) {
-    return Promise.reject(err.message ? err.message : data)
+  } catch (error) {
+    return error;
   }
 }
 
@@ -38,13 +28,12 @@ export async function redditGet(endpoint) {
   try {
     // Get an access token
     const { access_token } = await request(tokenURL, tokenConfig);
-    
+
     // Make a GET request
     const getConfig = {
       method: 'GET',
       headers: {
         'Authorization': `bearer ${access_token}`,
-        'User-Agent': appSignature
       }
     }
     return await request(baseURL + endpoint, getConfig);
