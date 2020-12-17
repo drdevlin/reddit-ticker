ef74898ab303b60665794972753fc8e74e499187
drdevlin <drdevlin@fastmail.com>
2020-12-17 12:16:34 -0500

    Inject post's text if there is some

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { decode } from 'he';
+import DOMPurify from 'dompurify';
 
 export const Post = (props) => {
   const post = props.post;
+  const decodedSelftextHtml = decode(post.selftext_html);
+  const body = DOMPurify.sanitize(decodedSelftextHtml);
+  
   return (
     <article>
       <h2>{post.title}</h2>
-      
+      {Boolean(body) && <div dangerouslySetInnerHTML={{__html: body}}></div>}
     </article>
   );
 }
