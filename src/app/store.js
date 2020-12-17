3e830d6a08efd5fd1301e94cb167d5da1c7d1f6f
drdevlin <drdevlin@fastmail.com>
2020-12-17 10:28:02 -0500

    Remove template code

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import topReducer from '../features/top/topSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     top: topReducer,
   },
 });
