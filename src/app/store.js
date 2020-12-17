23ca5db0f01eb821641329286bd344be3e76b711
drdevlin <drdevlin@fastmail.com>
2020-12-17 10:41:38 -0500

    Add hot slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import topReducer from '../features/top/topSlice';
+import hotReducer from '../features/hot/hotSlice';
 
 export default configureStore({
   reducer: {
     top: topReducer,
+    hot: hotReducer
   },
 });
