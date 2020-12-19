fb68277c3b9f403b4910b8c37ca9de79fa6aad95
drdevlin <drdevlin@fastmail.com>
2020-12-18 19:14:43 -0500

    Create initial header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Top } from './features/top/Top';
 import { Hot } from './features/hot/Hot';
 import { NewContainer } from './features/new/NewContainer';
+import { Header } from './features/header/Header';
 import './App.css';
 
 function App() {
   return (
     <main className='App'>
+      <div className='app-header'><Header /></div>
       <div className='app-top'><Top /></div>
       <div className='app-hot'><Hot /></div>
       <div className='app-new'><NewContainer /></div>  
